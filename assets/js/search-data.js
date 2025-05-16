81e17ef7b177d665c0eee790693c7b8d32a1e723
init-neok <64076369+init-neok@users.noreply.github.com>
2025-05-16 12:38:53 +0000

    Deploying to gh-pages from @ init-neok/init-neok.github.io@7529c14324c8876d97d408ca615c77ace94a02e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -493,7 +493,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%65%6F%6B%32%7A%6B%6E@%67%6D%61%69%6C.%63%6F%6D# %79%6F%75%72 %65%6D%61%69%6C %61%64%64%72%65%73%73", "_blank");
         },
       },{
         id: 'social-github',
@@ -536,13 +536,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://twitter.com/Neok2000", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
